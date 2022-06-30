53da22edf0cd1f7d9cb37b919053b3367a5d7bb8
angelgiovanna <aescobar2045@gmail.com>
2022-06-29 20:59:10 -0400

    added schedule route

---
+++ server/routes/api/schedule-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const { getAllOwners, addOwners, getOwnerPhoneById, addPhone, deletePhone, getAllDates, addDates, updateDate, deleteDate } = require('../../controllers/horse');
+
+router
+.route('/')
+.get(getAllOwners)
+.post(addOwners);
+
+router
+.route('/:owner')
+.get(getOwnerPhoneById)
+.post(addPhone)
+.delete(deletePhone);
+
+router
+.route('/:owner/schedule')
+.get(getAllDates)
+.post(addDates)
+.put(updateDate)
+.delete(deleteDate);
