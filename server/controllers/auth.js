6519cd90d39d68c15c57c974e6d0e3f31f5a9c71
Sarah Thompson <sthompson@fintechprocessing.com>
2022-07-01 14:55:53 -0400

    added bcrypt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,16 @@
 const User = require("../models/users.js");
+const bcrypt = require("bcrypt");
 
 //register a new user 
 const register = async (req, res, next) => {
     try{
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hash,
         })
 
         //if its successful
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const horseRoutes = require("./horse-routes")
 const authRoutes = require("./auth");
+const bcrypt = require("bcrypt");
 
 router.use("/user", userRoutes);
 router.use("/horse", horseRoutes);
