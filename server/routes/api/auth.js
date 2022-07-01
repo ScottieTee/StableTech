fedc4e64983d23f78587ac4fdc1cce653f7bb8f4
Sarah Thompson <sthompson@fintechprocessing.com>
2022-06-30 20:52:48 -0400

    cleaning up comments

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const register = require("../../controllers/auth");
 
-
-//router.post("/register", register)
-
 router.route("/register")
 .post(register)
 
