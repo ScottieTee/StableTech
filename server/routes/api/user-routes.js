075133b1daf06a4d4330596c650e5984b16f9c38
Sarah Thompson <sthompson@fintechprocessing.com>
2022-07-03 18:01:30 -0400

    trying to get the verify token to work

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const {totalUsers, newUser, getUserById, deleteUser} = require("../../controllers/user");
+const { verifyToken } = require("../../utils/verifytoken");
 
 router.route("/")
 .get(totalUsers)
@@ -8,5 +9,8 @@ router.route("/")
 router.route("/:id")
 .get(getUserById)
 .delete(deleteUser)
+
+router.route("/checkauthentication")
+.get(verifyToken)
 
 module.exports = router;
--- server/utils/verifytoken.js
+++ server/utils/verifytoken.js
@@ -8,7 +8,7 @@ const verifyToken = (req, res, next) => {
         return next(createError(401, "Not authenticated!"))
     }
 
-    JWT.verify(token,process.env.JWT, (err, user)=>{
+    JWT.verify(token, process.env.JWT, (err, user)=>{
         if(err)
         return next(createError(403, "Token is not valid!"));
         req.user = user;
