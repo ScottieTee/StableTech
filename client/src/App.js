5993413a1863c405b9992e3e49c0c6bd00c79873
Sarah Thompson <sthompson@fintechprocessing.com>
2022-07-06 19:28:55 -0400

    pages work

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './components/Login/index';
 import Horses from './components/Horse/index';
 import Schedule from './components/schedule/index';
 import Home from './pages/index';
-
-
-
 
 
 const httpLink = createHttpLink({
@@ -23,17 +19,33 @@ const client = new ApolloClient({
 });
 function App() {
   return (
-    <ApolloProvider client={client}>
-    <div>
-      <main>
-        
-     <Login></Login> 
-     <Home></Home>
-     <Horses></Horses>
-     <Schedule></Schedule>
-
-      </main>
-    </div>
+   <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/horses"
+                element={<Horses />}
+              />
+              <Route
+                path="/schedule"
+                element={<Schedule />}
+              />
+              </Routes>
+          </div>
+         
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
