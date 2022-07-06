3a526a53adcd97db98fad90e28b51776d67837ab
Sarah Thompson <sthompson@fintechprocessing.com>
2022-07-06 07:37:59 -0400

    trying to fix home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/footer';
+import Navbar from './components/navbar.jsx';
+import Hero from './components/hero';
 
 function App() {
   return (
     <div className="App">
       <Footer/>
+      <Navbar/>
+      <Hero/>
     </div>
   );
 }
