ef2af1f18047da310ee71999b19d7499ababc816
Sarah Thompson <sthompson@fintechprocessing.com>
2022-07-09 14:46:06 -0400

    trying to link homepage in the navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import barnlogo from '../../assets/images/barnlogo.png';
 import { Link } from 'react-scroll';
+import { useHistory } from 'react-router-dom';
 
 const Home = () => {
-return (
-<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#5b5b5b] opacity-75 text-gray-300">
-<div>
-    <img className="rounded-full" src={barnlogo} alt="logo" style={{ width: "75px"}} />
-</div>
+    //const history = useHistory();
+   // const backHome = () => {
+       // history.push("/home");
+   // }
+    return (
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#5b5b5b] opacity-75 text-gray-300">
+            <div>
+                <img className="rounded-full" src={barnlogo} alt="logo" style={{ width: "75px" }} />
+            </div>
 
 
-<ul className="hidden md:flex flex space-x-4">
-    <li className="hover:text-[#3faec1] hover:scale-105">
-        <Link to="hero" smooth={true} duration={500}>
-            Home
-        </Link>
-    </li>
-    <li className="hover:text-[#3faec1] hover:scale-105">
-        <Link to="about" smooth={true} duration={500}>
-            Sign In
-        </Link>
-    </li>
-</ul>
+            <ul className="hidden md:flex flex space-x-4">
+                <li className="hover:text-[#3faec1] hover:scale-105">
+                  
+                </li>
+                <li className="hover:text-[#3faec1] hover:scale-105">
+                    <Link to="about" smooth={true} duration={500}>
+                        Sign In
+                    </Link>
+                </li>
+            </ul>
 
-</div>
-);
+        </div>
+    );
 };
 
 export default Home;
